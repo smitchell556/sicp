19ae607b0ea6f6a8010761982a87050e7fd7fdb4
Spencer Mitchell <smitchell556@gmail.com>
2017-03-15 10:08:56 -0700

    sec1.1 problems

--- ch1/sec1-1.rkt
+++ ch1/sec1-1.rkt
@@ -4,6 +4,7 @@
 ;;; Section 1.1
 ;;; -----------
 
+
 ;;; Exercise 1.1
 ;;; What are the results printed by the interpreter for each expression?
 
@@ -50,3 +51,35 @@
          (else -1))
    (+ a 1))
 ;; 16
+
+
+;;; Exercise 1.2
+;;; Convert equation to prefix form
+
+;; infix: ((5 + 4 + (2 - ( 3 - (6 + (4/5))))) / (3 * (6 - 2) * (2 - 7))) = -37/150
+;; prefix: (/ (+ 5 (+ 4 (- 2 (- 3 (+ 6 (/ 4 5)))))) (* 3 (* (- 6 2) (- 2 7))))
+;;         = / + 5 + 4 - 2 - 3 + 6 / 4 5 * 3 * - 6 2 - 2 7
+
+;; converting prefix to scheme will remove some of the repeated operators
+
+(/ (+ 5
+      4
+      (- 2
+         (- 3
+            (+ 6
+               (/ 4 5)))))
+   (* 3
+      (- 6 2)
+      (- 2 7)))
+;; -37/150
+
+
+;;; Exercise 1.3
+;;; Define a procedure that takes three numbers and returns the sum of squares
+;;; of largest two.
+
+(define (sum-largest-two-squares x y z)
+  (cond
+   ((<= z x y) (+ (* x x) (* y y)))     ; if z is smallest return x^2 + y^2
+   ((<= y x z) (+ (* x x) (* z z)))     ; if y is smallest return x^2 + z^2
+   (else (+ (* y y) (* z z)))))         ; else x must be smallest so return y^2 + z^2
