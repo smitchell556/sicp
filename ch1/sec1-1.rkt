cc9a1750c05839a5ba82187c2145397ef375c2ff
Spencer Mitchell <smitchell556@gmail.com>
2017-03-16 10:15:15 -0700

    1.4 and 1.5 ex done

--- ch1/sec1-1.rkt
+++ ch1/sec1-1.rkt
@@ -83,3 +83,43 @@
    ((<= z x y) (+ (* x x) (* y y)))     ; if z is smallest return x^2 + y^2
    ((<= y x z) (+ (* x x) (* z z)))     ; if y is smallest return x^2 + z^2
    (else (+ (* y y) (* z z)))))         ; else x must be smallest so return y^2 + z^2
+
+
+;;; Exercise 1.4
+;;; Describe the behavior of the following procedure:
+
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+
+;; The if statement returns a procedure (either + or -) which will be used to
+;; evaluate the following operands (a and b). If b is positive, the + operator
+;; is returned and (+ a b) is evaluated, otherwise the - operator is returned
+;; and (- a b) is evaluated. The result of that evaluation is returned from
+;; the function.
+
+
+;;; Exercise 1.5
+;;; What behavior will be observed for the following procedures/evaluations if
+;;; the interpreter uses applicative-order evaluation or normal-order
+;;; evaluation?
+
+(define (p) (p))
+
+(define (test x y)
+  (if (= x 0)
+      0
+      y))
+
+(test 0 (p))
+
+;; If the interpreter uses applicative-order evaluation, this will result in
+;; an infinite loop. (p) will be evaluated before (test 0 (p)), which will
+;; call itself as (p) continually until the program is killed.
+
+;; If the interpreter uses normal-order evaluation, then the arguments passed
+;; to (test) won't be evaluated until the procedure has fully expanded. So
+;; (test 0 (p)) will not evaluate (p) immediately, but will instead begin
+;; executing the (test) procedure. Since x is 0, 0 is returned and (p) is never
+;; evaluated.
+
+;; This test determines the order evaluation of the interpreter.
