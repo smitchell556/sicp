1b84e73b21e3190f626edba64da73bda61fa8578
Spencer Mitchell <smitchell556@gmail.com>
2017-03-24 10:30:07 -0700

    finished ex 1.10

--- ch1/sec1-2.rkt
+++ ch1/sec1-2.rkt
@@ -129,7 +129,7 @@
 
 (A 3 3)
 ;; (A 2 (A 3 2))
-;; (A 2 (A 2 (A 2 1)))
+;; (A 2 (A 2 (A 3 1)))
 ;; (A 2 (A 2 2))
 ;; (A 2 (A 1 (A 2 1)))
 ;; (A 2 (A 1 2))
@@ -181,3 +181,16 @@
 ;; (A 0 (A 0 16384))
 ;; (A 0 32768)
 ;; 65536
+
+;;; Consider the following procedures, where A is the procedure defined above,
+;;; what are the mathematical definitions for the functions computed by the
+;;; procedures f, g, and h?
+
+(define (f n) (A 0 n))
+;; (f n) = 2 * n
+
+(define (g n) (A 1 n))
+;; (g n) = 2^n
+
+(define (h n) (A 2 n))
+;; (h n) = 2^2^2^(y-2) or 2^(2^...(n-1 times)) (total of n 2's)
