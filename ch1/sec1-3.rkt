877de2067bdfb8bb1043dfd19e0141d3ccfc1f67
Spencer Mitchell <smitchell556@gmail.com>
2017-06-27 11:22:49 -0700

    finished sec 1.3.3

--- ch1/sec1-3.rkt
+++ ch1/sec1-3.rkt
@@ -295,7 +295,8 @@
 ;;; Exercise 1.37
 ;;; -------------
 ;;; a. Define a procedure cont-frac that calculates a continued fraction
-;;;    up to k terms.
+;;;    up to k terms. How large must k be in order to get an approximation
+;;;    that is accurate to 4 decimal places? 1/(golden ratio) =~ 0.618033987
 
 (define (cont-frac n d k)
   (define (cont-frac-counter i)
@@ -303,3 +304,55 @@
 	(/ (n i) (d i))
 	(+ (d (- i 1)) (/ (n i) (cont-frac-counter (+ i 1))))))
   (/ (n 1) (cont-frac-counter 2)))
+
+(cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 11)
+;; 0.6179775280898876
+
+(cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 12)
+;; 0.6180555555555556
+
+;; k >= 12
+
+;;; b. Write cont-frac as an iterative process.
+
+(define (cont-frac n d k)
+  (define (cont-frac-iter k rv)
+    (if (= k 1)
+	(/ (n k) (+ (d k) rv))
+	(cont-frac-iter (- k 1) (/ (n k) (+ (d k) rv)))))
+  (cont-frac-iter (- k 1) (/ (n k) (d k))))
+
+
+;;; Exercise 1.38
+;;; -------------
+;;; Write a procedure that approximates e based on cont-frac.
+
+(define (approx-e k)
+  (+ 2
+     (cont-frac (lambda (i) 1.0)
+		(lambda (i) (if (= (remainder i 3) 2)
+				(* 2
+				   (/ (+ i 1)
+				      3))
+				1))
+		k)))
+
+
+;;; Exercise 1.39
+;;; -------------
+;;; Write a procedure that approximates the tangent function based on cont-frac.
+
+;; Note the tangent continued fraction uses subtraction instead of addition.
+(define (cont-frac n d k)
+  (define (cont-frac-iter k rv)
+    (if (= k 1)
+	(/ (n k) (- (d k) rv))
+	(cont-frac-iter (- k 1) (/ (n k) (- (d k) rv)))))
+  (cont-frac-iter (- k 1) (/ (n k) (d k))))
+
+(define (tan-cf x k)
+  (cont-frac (lambda (i) (if (= i 1)
+			     x
+			     (* x x)))
+	     (lambda (i) (+ i (- i 1)))
+	     k))
