492c069f7a1b3a1b158af92171e1e032dff7e0a4
Spencer Mitchell <smitchell556@gmail.com>
2017-06-29 10:54:52 -0700

    fixed a few slips that crept in

--- ch1/sec1-3.rkt
+++ ch1/sec1-3.rkt
@@ -71,7 +71,7 @@
   (define (iter a result)
     (if (> a b)
 	result
-	(iter (next a) (+ result a))))
+	(iter (next a) (+ result (term a)))))
   (iter a 0))
 
 
@@ -86,12 +86,12 @@
       (* (term a)
 	 (product term (next a) next b))))
 
-(define (factorial a b)
+(define (factorial b)
   (define (identity x)
     x)
   (define (inc x)
     (+ x 1))
-  (product identity a inc b))
+  (product identity 1 inc b))
 
 (define (pi-prod a b)
   (define (pi-func x)
