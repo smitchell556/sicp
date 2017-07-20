917949b53f5f8ae1a17c53a7f1b521dde5bf0079
Spencer Mitchell <smitchell556@gmail.com>
2017-07-20 10:11:55 -0700

    finished exercise 2.2

--- ch2/sec2-1.rkt
+++ ch2/sec2-1.rkt
@@ -33,3 +33,51 @@
 (define (make-rat n d)
   (let ((g (gcd n d)))
     (cons (equalize-sign (/ n d) (/ n g)) (equalize-sign 1 (/ d g)))))
+
+
+;;; Exercise 2.2
+;;; ------------
+;;; Represent lines using two points on a plane. Define make-segment which has
+;;; a start-segment and end-segment. A point can be represented as an
+;;; x-coordinate and y-coordinate. Define make-point and selectors x-point and
+;;; y-point. Define a procedure midpoint-segment which takes a line segment and
+;;; returns its midpoint (the average of the coordinates of the endpoints.
+
+;; Supplied is a procedure to print points.
+
+(define (print-point p)
+  (display "(")
+  (display (x-point p))
+  (display ", ")
+  (display (y-point p))
+  (display ")")
+  (newline))
+
+;; point structure API
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point p)
+  (car p))
+
+(define (y-point p)
+  (cdr p))
+
+;; line segment structure API
+
+(define (make-segment start end)
+  (cons start end))
+
+(define (start-segment s)
+  (car s))
+
+(define (end-segment s)
+  (cdr s))
+
+;; midpoint definition
+(define (midpoint-segment s)
+  (define (avg-coord segment coord)
+    (/ (+ (coord (start-segment segment)) (coord (end-segment segment)))
+       2))
+  (make-point (avg-coord s x-point) (avg-coord s y-point)))
