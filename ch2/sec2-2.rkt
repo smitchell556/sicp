6375e8cd04a10f2047a418f93219a29549808b9c
Spencer Mitchell <smitchell556@gmail.com>
2017-10-17 16:23:37 -0700

    finished 2.41

--- ch2/sec2-2.rkt
+++ ch2/sec2-2.rkt
@@ -620,3 +620,79 @@
 (define (triplet-sum n s)
   (filter (lambda (p) (= s (accumulate + 0 p)))
 	  (unique-triplets n)))
+
+
+;;; Exercise 2.42
+;;; -------------
+;;; Write procedures for queens: adjoin-position, empty-board, and safe?
+
+;; Given:
+
+(define (queens board-size)
+  (define (queen-cols k)  
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) '())
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(define (flatmap proc seq)
+  (accumulate append '() (map proc seq)))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      '()
+      (cons low (enumerate-interval (+ low 1) high))))
+
+;; ----
+
+(define (make-queen row col)
+  (cons row col))
+
+(define (queen-row queen)
+  (car queen))
+
+(define (queen-col queen)
+  (cdr queen))
+
+(define empty-board '())
+
+(define (adjoin-position row column positions)
+  (append positions (list (make-queen row column))))
+
+(define (safe? col positions)
+  (define (iter-safe? row col positions)
+    (if (null? positions)
+	#t
+	(let ((pos (car positions)))
+	  (cond
+	   ((= (cdr pos) col) (iter-safe? row col (cdr positions)))
+	   ((= (car pos) row) #f)
+	   ((or (= (- (car pos) (cdr pos))
+		   (- row col))
+		(= (+ (car pos) (cdr pos))
+		   (+ row col)))
+	    #f)
+	   (else (iter-safe? row col (cdr positions)))))))
+  
+  (let ((queen (car (filter (lambda (q) (= (queen-col q) col)) positions))))
+    (iter-safe? (queen-row queen) (queen-col queen) positions)))
