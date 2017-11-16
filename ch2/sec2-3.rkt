672ac9d5a464c8cc5a0b528c02a5373754cadae6
Spencer Mitchell <smitchell556@gmail.com>
2017-11-16 10:54:44 -0800

    finished ex 2.66

--- ch2/sec2-3.rkt
+++ ch2/sec2-3.rkt
@@ -627,3 +627,25 @@
 
 (define (intersection-set set1 set2)
   (set-operation intersection-set-ordered-list set1 set2))
+
+
+;;; Exercise 2.66
+;;; -------------
+;;; Implement the lookup procedure for the case where the set of records is
+;;; structured as a binary tree, ordered by the numerical values of the keys.
+
+;; Given:
+
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+
+;; Solution:
+
+(define (lookup given-key set-of-records)
+  (cond ((null? set-of-records) false)
+	((equal? given-key (key (entry set-of-records))) (entry set-of-records))
+	((< given-key (key (entry set-of-records))) (lookup given-key (left-branch set-of-records)))
+	(else (lookup given-key (right-branch set-of-records)))))
